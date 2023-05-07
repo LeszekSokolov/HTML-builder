9dfe94666b976aeedd900e8e6b00c4f3c4466a9f
LeszekSokolov <Leszek.Sokolov@gmail.com>
2023-05-07 12:14:48 +0300

    feat: complete copy directory task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,20 @@
+const path = require("path");
+const fs = require("fs/promises");
+let directory = path.join(__dirname, "files");
+let copy = path.join(__dirname, "files-copy");
 
+fs.rm(copy, {
+  recursive: true,
+  force: true,
+}).finally(function () {
+  fs.mkdir(copy, { recursive: true });
+  fs.readdir(directory, { withFileTypes: true }).then(function (data) {
+    data.forEach(function (file) {
+      if (file.isFile()) {
+        let pathFile = path.join(directory, file.name);
+        let pathFileCopy = path.join(copy, file.name);
+        fs.copyFile(pathFile, pathFileCopy);
+      }
+    });
+  });
+});
